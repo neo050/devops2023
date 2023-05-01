044415d17a3d486e59582ca299614fddaf6a4320
neo050 <neoray.asking666@gmail.com>
2023-05-01 09:56:45 +0300

    x

--- tests/test1.test.js
+++ tests/test1.test.js
@@ -1,14 +1,14 @@
-const supertest = require("supertest");
+const supertest = require('supertest');
 
 const app =require('../server')
 
 describe("test suit 1:",()=>{
-    test('test1 1:',async()=>{
+    test("test1 1: ",async()=>{
         const res = await request(app).get('/')
         expect(res.statusCode.toEqual(200))
 
     } )
-    test('test1 2:',async()=>{
+    test("test1 2: ",async()=>{
         const res = await request(app).get('/1234')
         expect(res.statusCode.toEqual(404))
 
