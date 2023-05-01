66ff0188a2cfb75769b4a4e10d68f67c928f1810
neo050 <neoray.asking666@gmail.com>
2023-05-01 09:04:38 +0300

    new

---
+++ tests/test1.test.js
@@ -0,0 +1,16 @@
+const supertest = require("supertest");
+
+const app =require('../server')
+
+describe("test suit 1:",()=>{
+    test('test1 1:',async()=>{
+        const res = await request(app).get('/')
+        expect(res.statusCode.toEqual(200))
+
+    } )
+    test('test1 2:',async()=>{
+        const res = await request(app).get('/1234')
+        expect(res.statusCode.toEqual(404))
+
+    } )
+})
